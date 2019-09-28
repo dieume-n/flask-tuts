e65d293781c92a2baf912f4fea06f4dbfb17571f
= <=>
2019-09-28 12:21:49 +0300

    initial commit

---
+++ tuts/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '98d9a5faf472ba73ad335a8754d6e09e785871db1f83dad65557824dc466cc35'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from tuts import routes
